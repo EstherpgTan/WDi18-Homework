2cecf0b9d2878d71bb3db666cb027985e38897ca
estherpgtan <estherpgtan@gmail.com>
2016-10-28 09:12:33 +1100

    geometry function lab

---
+++ esther_tan/week_01/20161027/main.js
@@ -0,0 +1,52 @@
+//Geometry Function Lab
+
+//Part 1, Rectangle
+
+//Given the following a rectangle object like the one below, write the following functions:
+
+//isSquare - Returns whether the rectangle is a square or not
+//area - Returns the area of the rectangle
+//perimeter - Returns the perimeter of the rectangle
+
+var isSquare = Function(4) {
+var isSquare = 4 * 4;
+return isSquare;
+};
+var squareOfFour = squareNumber (4);
+var squareOfTwelve = squareNumber (12);
+squareNumber (4) + squareNumber (12);
+
+var area = Function multiplyNumbers (4 * 4);
+var perimeter = Function addNumbers (4,4,4,4); {
+};
+
+//Part 2, Triangle
+
+//Given the following a triangle object like the one below, write the following functions:
+
+//isEquilateral - Returns whether the triangle is equilateral or not
+//isIsosceles - Returns whether the triangle is isosceles or not
+//area - Returns the area of the Triangle
+//isObtuse - Returns whether the triangle is obtuse or not
+
+// 3 equal sides
+var isEquilateral = Function equal sides (sideA = sideB = sideC){
+var isEquilateral = Function equal sides (3 = 4 = 4);
+return isNotEquilateral;
+};
+
+//2 sides of equal length
+var isIsosceles = Function equal sides (sideB = sideC){
+var isIsosceles = Function equal sides (4 = 4) ;
+return isIsosceles;
+};
+
+var area = Function areaOftriangle multiplyNumbers (0.5 * 3 * 3.71);
+return areaOftriangle;
+};
+
+//Obtuse triangle has 1 angle greater than 90 degrees & 2 acute angles
+//Isosceles has 3 equal angles of 60 degrees
+var isObtuse = Function angle < 90 degrees;
+return notObtuse;
+};
