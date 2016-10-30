eb38ef202bfd3f8515d5ec784f8ef470c6a8732f
estherpgtan <estherpgtan@gmail.com>
2016-10-31 09:07:17 +1100

    mtalab

---
+++ esther_tan/week_01/20161028/main.js
@@ -0,0 +1,81 @@
+//Activity
+
+//Create a program that models a simple subway system.
+
+//The program takes the line and stop that a user is getting on at and the line and stop that user is getting off at and prints the journey and the total number of stops for the trip in the console:
+
+//planTrip('N', 'Times Square', '6', '33rd'); // This is only a suggested function name and signature.
+
+// console.log() shows output similar to this:
+// "Your must travel through the following stops on the N line: 34th, 28th, 23rd, Union Square."
+// "Change at Union Square."
+// "Your journey continues through the following stops: 23rd, 28th, 33rd."
+// "7 stops in total."
+
+//There are 3 subway lines:
+//The N line has the following stops: Times Square, 34th, 28th, 23rd, Union Square, and 8th
+//The L line has the following stops: 8th, 6th, Union Square, 3rd, and 1st
+//The 6 line has the following stops: Grand Central, 33rd, 28th, 23rd, Union Square, and Astor Place.
+//All 3 subway lines intersect at Union Square, but there are no other intersection points. (For example, this means the 28th stop on the N line is different than the 28th street stop on the 6 line, so you'll have to differentiate this when you name your stops in the arrays.)
+//Tell the user the number of stops AND the stops IN ORDER that they will pass through or change at.
+
+//Get really good at array manipulation.
+
+//Hints:
+
+//Work out how you would do it on paper first! Then start to explain that process in Javascript.
+//Get the program to work for a single line before trying to tackle multiple lines.
+//Don't worry about prompting the user for input. Hard code some values to get it working. You can use prompt() later to make it more interactive.
+//Consider diagramming the lines by sketching out the subway lines and their stops and intersection.
+//The key to the lab is finding the index positions of each stop. (hint: indexOf())
+//Make sure the stops that are the same for different lines have different names (i.e. 23rd on the N and on the 6 need to be differentiated)
+
+console.log ("HelloWorld");
+//N line
+var Stops ["Times Square", "34 stop", "29 stop", "23 stop"];
+console.log (stops.[4]);
+//4
+
+for (Times Square; 34 stop;){
+}
+for (var i = 0; i <=4; i+1 {
+  console.log (stops.[1]);
+}
+for (34 stop; 29 stop;){
+}
+for (var i=1); i<=4; i+2 {
+  console.log (stops.[2]);
+}
+for (29 stop; 23 stop;){
+}
+for (var i=2); i<=4; i+3 {
+  console.log (stops.[3]);
+}
+
+//L line: Change
+var stop ["Union Square"];
+console.log (stops.[1]);
+//1
+//??
+
+//6 line
+var 6 stops ["23 stop", "28 stop", "33 stop"];
+console.log(stops[3]);
+//3
+
+for (23 stop; 28 stop;){
+}
+for (var i = 0; i <=4; i+1 {
+  console.log (stops.[1]);
+}
+for (28 stop; 33 stop;){
+}
+for (var i = 1; i < array.[2]; i+1) {
+  console.log (stops.[2]);
+}
+
+
+
+
+
+//6 line
