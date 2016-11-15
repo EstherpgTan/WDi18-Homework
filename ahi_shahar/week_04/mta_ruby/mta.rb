efff5fa4caf35ba5f00f10d24acef7343bbf9764
AhiShahar <ahishahar1987@gmail.com>
2016-11-15 23:14:55 +1100

    cleaned it up a bit and added the single line trip

--- ahi_shahar/week_04/mta_ruby/mta.rb
+++ ahi_shahar/week_04/mta_ruby/mta.rb
@@ -38,22 +38,51 @@ def trim_lines (line, station, interchange, first_part_of_trip)
   return new_line
 end
 
-while (travelling == 'Y')
+def call_user
   print "which station are you travelling from? "
     start = gets.chomp()
   print "Which station are you traveling to? "
     stop = gets.chomp()
-  start_line,name_start_line = find_line(start)
-  end_line,name_end_line = find_line(stop)
+    return start,stop
+end
+
+def interchange_and_stops(start_line,start,end_line,stop)
   interchange = find_interchange(start_line,end_line)[0]
   start_line = trim_lines(start_line, start, interchange, true) # check directions
   end_line = trim_lines(end_line, stop, interchange, false) # for both lines
-  total_stops = start_line.length + end_line.length - 1
-  puts "\n \t You are traveling from #{start} on the #{name_start_line}, \n
-  continue through #{start_line.join(", ")} and change trains to #{name_end_line} at: #{interchange}. \n
-  continue your journey through #{end_line.join(", ")} all the way to #{stop}. \n
-  you have #{total_stops} on your journey, good luck.\n"
-  puts ""
-  print "Would you like to plan another trip? Y/N "
+  total_stops = start_line.length + end_line.length
+  return interchange, start_line, end_line, total_stops
+end
+
+def single_line_trip(start_line,start,stop)
+  start_index = start_line.index(start)
+  stop_index = start_line.index(stop)
+  start_line.reverse! if(start_index > stop_index)
+  start_index = start_line.index(start)
+  stop_index = start_line.index(stop)
+  line = start_line.slice!((start_index+1)..(stop_index-1))
+  return line
+end
+
+while (travelling == 'Y')
+  start, stop = call_user
+
+  start_line,name_start_line = find_line(start)
+  end_line,name_end_line = find_line(stop)
+
+  if name_start_line != name_end_line
+    interchange, start_line, end_line, total_stops = interchange_and_stops(start_line,start,end_line,stop)
+    puts "\n \t You are traveling from #{start} on the #{name_start_line}, \n
+    continue through #{start_line.join(", ")} and change trains to #{name_end_line} at: #{interchange}. \n
+    continue your journey through #{end_line.join(", ")} all the way to #{stop}. \n
+    You have #{total_stops} on your journey, good luck.\n"
+  else
+    line = single_line_trip(start_line,start,stop)
+    puts "\n \t You are traveling on line #{name_start_line} from #{start}. \n
+    contine through #{line.join(", ")} all the way to #{stop}.\n
+    You have #{line.length+1} stops on your way, good luck!\n"
+  end
+    puts ""
+    print "Would you like to plan another trip? Y/N "
   travelling = gets.chomp()
 end
