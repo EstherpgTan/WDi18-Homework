f0cfc8c10a0f40decbd19768e444cd276b5a5fe0
estherpgtan <estherpgtan@gmail.com>
2016-12-11 23:51:23 +1100

    Todos

---
+++ esther_tan/week_07/20161209/backbone-Todos/js/main.js
@@ -0,0 +1,29 @@
+// Backbone Homework
+//
+// Read the getting started guide on Backbone.js
+// This is the main piece of homework - finish at least Chapter 1 of this Todo app by Addy Osmani, making sure to read the introduction
+// Read the following on cdnJS:
+// Why do you need Backbone?
+// What is a model?
+// What is a collection?
+// What is a view?
+// What is a router?
+// Read the Sitepoint introduction to Backbone
+// If you have an account, do these tutorials on CodeSchool
+// Other Homework
+//
+// Start thinking about potential group project ideas
+// Start thinking about your personal portfolio
+
+
+// The application’s architecture at a high level. We’ll need:
+//
+// a Todo model to describe individual todo items
+// a TodoList collection to store and persist todos
+// a way of creating todos
+// a way to display a listing of todos
+// a way to edit existing todos
+// a way to mark a todo as completed
+// a way to delete todos
+// a way to filter the items that have been completed or are remaining
+// Essentially, these features are classic CRUD methods. Let’s get started!
